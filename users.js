a2869f89941a6fc699a8af389910c928a17d87fa
Настольний Антон Олександрович <a.nastolnyi@officeukraine.fozzy.lan>
2019-12-13 17:53:16 +0200

    test

---
+++ users.js
@@ -0,0 +1,213 @@
+var users = [
+    {
+        "id": 1,
+        "name": "Анастасія Кравець",
+        "avatar": "anastasiaKravets.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 2,
+        "name": "Анатолій ",
+        "avatar": "anatoliyBuhera.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 3,
+        "name": "",
+        "avatar": "antonNastolnyi.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 4,
+        "name": "",
+        "avatar": "bohdanLuchenko.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 5,
+        "name": "",
+        "avatar": "bohdanSemykras.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 6,
+        "name": "",
+        "avatar": "bohdanSvec.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 7,
+        "name": "",
+        "avatar": "dianaKlimchik.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 8,
+        "name": "",
+        "avatar": "dima__.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 9,
+        "name": "",
+        "avatar": "dimaBilukha.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 10,
+        "name": "",
+        "avatar": "dimaSamborskyi.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 11,
+        "name": "",
+        "avatar": "evgeniyDolhiy.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 12,
+        "name": "",
+        "avatar": "igorPedorych.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 13,
+        "name": "",
+        "avatar": "ilonaKruzanivska.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 14,
+        "name": "",
+        "avatar": "ivanMukutiyk.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 15,
+        "name": "",
+        "avatar": "JuliaBurlachuk.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 16,
+        "name": "",
+        "avatar": "JuliaStankevich.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 17,
+        "name": "",
+        "avatar": "ludaKomarova.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 18,
+        "name": "",
+        "avatar": "MaksimBorschevskiy.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 19,
+        "name": "",
+        "avatar": "maksymKapliuk.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 20,
+        "name": "",
+        "avatar": "olehKorolenko.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 21,
+        "name": "",
+        "avatar": "olehZasadniuk.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 22,
+        "name": "",
+        "avatar": "pavloStoliaryk.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 23,
+        "name": "",
+        "avatar": "sashaFedorchuk.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 24,
+        "name": "",
+        "avatar": "sashaTiapkin.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 25,
+        "name": "",
+        "avatar": "sergiyLevchenko.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 26,
+        "name": "",
+        "avatar": "sergiyUstinov.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 27,
+        "name": "",
+        "avatar": "Veronika.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 28,
+        "name": "",
+        "avatar": "viktoriaPosvalyuk.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 29,
+        "name": "",
+        "avatar": "vitaliiSnitsar.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 30,
+        "name": "",
+        "avatar": "vladChegarovskiy.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 31,
+        "name": "",
+        "avatar": "vladimir.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 32,
+        "name": "",
+        "avatar": "vladimirIvakh.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 33,
+        "name": "",
+        "avatar": "vladimirNorcov.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 34,
+        "name": "",
+        "avatar": "vladimirPustovit.png",
+        "wish": "",
+        "participation": null
+    },{
+        "id": 35,
+        "name": "",
+        "avatar": "yuriyPogoreliuk.png",
+        "wish": "",
+        "participation": null
+    }
+]
